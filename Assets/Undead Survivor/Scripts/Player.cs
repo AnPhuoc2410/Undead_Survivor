17e3c88a7196804767d6c8dc4edd4239bef9bea6
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-05-18 16:23:58 +0700

    feat: Animatior Tree

--- Assets/Undead Survivor/Scripts/Player.cs
+++ Assets/Undead Survivor/Scripts/Player.cs
@@ -4,19 +4,34 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public float speed = 5f; // Speed of the player
-    public Vector3 input;
-    Rigidbody2D rigidbody;
+    private Vector2 input;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private SpriteRenderer spriteRenderer;
 
 
     void Awake()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        // Update the animator with the current speed
+        animator.SetFloat("Speed", input.magnitude); //check is the player is moving
     }
 
     void FixedUpdate()
     {
-        // Move the player using Rigidbody2D for better physics handling
-        rigidbody.MovePosition(rigidbody.position + new Vector2(input.x, input.y) * speed * Time.fixedDeltaTime);
+        Vector2 move = input * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + move); //ideal for smooth movement
+
+        if (input != Vector2.zero)
+        {
+            spriteRenderer.flipX = input.x < 0;  //true if moving left - false if moving right
+        }
     }
 
     void OnMove(InputValue inputValue)
